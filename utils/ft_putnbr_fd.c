9095196b258a81b8109a441a43d4cd49c6edbfbd
Hotta Sachie <hsachie@c4r3s1.42tokyo.jp>
2026-05-25 20:25:11 +0900

    a

--- utils/ft_putnbr_fd.c
+++ utils/ft_putnbr_fd.c
@@ -1,31 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hsachie <hsachie@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/23 17:35:14 by hsachie           #+#    #+#             */
-/*   Updated: 2026/05/25 20:08:30 by hsachie          ###   ########.fr       */
+/*   Created: 2026/05/25 19:58:45 by hsachie           #+#    #+#             */
+/*   Updated: 2026/05/25 19:59:17 by hsachie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
-
-int	ft_printf(const char *format, ...)
+void ft_putnbr_fd(int n, int fd)
 {
-	va_list	ap;
-	int		i;
-	int		count;
-
-	i = 0;
-	count = 0;
-	va_start(ap, format);
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-		}
-	}
+	write (fd, n, 1);
 }
