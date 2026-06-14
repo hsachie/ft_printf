167f19fd6a60f32a8d73f9e2ef9597b56ba22596
hsachie <sachie.duck.1113@icloud.com>
2026-06-14 16:45:17 +0900

    完

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-*This project has been created as part of the 42 curriculum by haruya.*
+*This project has been created as part of the 42 curriculum by hasachie.*
 
 # ft_printf
 
--- test_mix.c
+++
@@ -1,7 +0,0 @@
-#include "ft_printf.h"
-#include <stdio.h>
-int main(void)
-{
-    int r = ft_printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42, 42, 42, 42, 'B', "-42", -42, -42, -42, -42, 42, 'C', "0", 0, 0, 0, 0, 42, 0);
-    printf("\nret: %d\n", r);
-}
